48aa74b049cd641b994c47a82b8a051ed820a66e
adenugbamichael <adenugbamicke@gmail.com>
2022-08-26 13:30:26 +0100

    implemented authorization role for the server side

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,6 +1,8 @@
 const { expressjwt: jwt } = require("express-jwt")
 const jwksRsa = require("jwks-rsa")
 
+const namespace = "http://localhost:3000/"
+
 // MIDDLEWARE
 exports.checkJWT = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -15,3 +17,15 @@ exports.checkJWT = jwt({
   issuer: "https://dev-1x2k7k7q.us.auth0.com/",
   algorithms: ["RS256"],
 })
+
+exports.checkRole = (role) => (req, res, next) => {
+  const user = req.user
+  if (user && user[namespace + "role"] === role) {
+    next()
+  } else {
+    return res.status(401).send({
+      title: "Not Authorized",
+      detail: "You are not authorized to access this data",
+    })
+  }
+}
