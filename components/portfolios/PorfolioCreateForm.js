593e6c1e916f8fb8887a287297a5bb140f96faa7
adenugbamichael <adenugbamicke@gmail.com>
2022-08-31 21:41:03 +0100

    Added Form and Date Picker components to portfoliocreateform

--- components/portfolios/PorfolioCreateForm.js
+++ components/portfolios/PorfolioCreateForm.js
@@ -1,66 +1,186 @@
 import React from "react"
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import { Button, Alert } from "reactstrap"
+import PortInput from "../form/PortInput"
+import PortDate from "../form/PortDate"
 
-export default class PortfolioCreatForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { title: "", description: "", language: "" }
+import moment from "moment"
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+const validateInputs = (values) => {
+  let errors = {}
+
+  Object.entries(values).forEach(([key, value]) => {
+    if (!values[key] && key !== "endDate") {
+      errors[key] = `Field ${key} is required!`
+    }
+  })
+
+  const startDate = moment(values.startDate)
+  const endDate = moment(values.endDate)
+
+  if (startDate && endDate && endDate.isBefore(startDate)) {
+    errors.endDate = "End Date cannot be before start date!!!"
   }
 
-  handleChange(event) {
-    this.setState({ title: event.target.value })
-  }
-
-  handleSubmit(event) {
-    alert(
-      "A name was submitted: " +
-        this.state.title +
-        " " +
-        this.state.description +
-        " " +
-        this.state.language +
-        " "
-    )
-    event.preventDefault()
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            name='title'
-            type='text'
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Description:
-          <textarea
-            name='description'
-            value={this.state.description}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Pick your favorite Programming Language:
-          <select
-            name='language'
-            value={this.state.language}
-            onChange={this.handleChange}
-          >
-            <option value='c++'>C++</option>
-            <option value='javascript'>Javascript</option>
-            <option value='nodejs'>Node</option>
-            <option value='typescript'>Typescript</option>
-          </select>
-        </label>
-        <input type='submit' value='Submit' />
-      </form>
-    )
-  }
+  return errors
 }
+
+const INITIAL_VALUES = {
+  title: "",
+  company: "",
+  location: "",
+  position: "",
+  description: "",
+  startDate: "",
+  endDate: "",
+}
+
+const PortfolioCreateForm = ({ initialValues, onSubmit, error }) => (
+  <div>
+    <Formik
+      initialValues={INITIAL_VALUES}
+      validate={validateInputs}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2))
+          setSubmitting(false)
+        }, 400)
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field label='Title' type='text' name='title' component={PortInput} />
+          <Field
+            label='Company'
+            type='text'
+            name='company'
+            component={PortInput}
+          />
+          <Field
+            label='Location'
+            type='text'
+            name='location'
+            component={PortInput}
+          />
+          <Field
+            label='Position'
+            type='text'
+            name='position'
+            component={PortInput}
+          />
+          <Field
+            label='Description'
+            type='textarea'
+            name='description'
+            component={PortInput}
+          />
+          <Field label='Start Date' name='startDate' component={PortDate} />
+          <Field label='End Date' name='endDate' component={PortDate} />
+          <button type='submit' disabled={isSubmitting}>
+            Create
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+)
+
+export default PortfolioCreateForm
+
+// Render Prop
+// import React from "react"
+// import { Formik, Form, Field, ErrorMessage } from "formik"
+// import { Button } from "reactstrap"
+// import PortInput from "../form/PortInput"
+// import PortDate from "../form/PortDate"
+
+// const validateInputs = (values) => {
+//   const errors = {}
+
+//   Object.entries(values).forEach(([key, value]) => {
+//     if (!values[key] && key !== "endDate") {
+//       errors[key] = `Field ${key} is required!`
+//     }
+//   })
+
+//   const startDate = values.startDate
+//   const endDate = values.endDate
+
+//   if (startDate && endDate && endDate.isBefore(startDate)) {
+//     errors.endDate = "End Date cannot be before start date!!"
+//   }
+//   return errors
+// }
+
+// const INITIAL_VALUES = {
+//   title: "",
+//   company: "",
+//   location: "",
+//   position: "",
+//   description: "",
+//   startDate: "",
+//   endDate: "",
+// }
+
+// const PortfolioCreatForm = (props) => (
+//   <div>
+//     <Formik
+//       initialValues={INITIAL_VALUES}
+//       validate={validateInputs}
+//       onSubmit={props.onSubmit}
+//     >
+//       {({ isSubmitting }) => (
+//         <Form>
+//           <Field type='text' name='title' label='Title' component={PortInput} />
+
+//           <Field
+//             type='text'
+//             name='company'
+//             label='Company'
+//             component={PortInput}
+//           />
+
+//           <Field
+//             type='text'
+//             name='location'
+//             label='Location'
+//             component={PortInput}
+//           />
+
+//           <Field
+//             type='text'
+//             name='position'
+//             label='Position'
+//             component={PortInput}
+//           />
+
+//           <Field
+//             type='textarea'
+//             name='description'
+//             label='Description'
+//             component={PortInput}
+//           />
+//           <Field name='startDate' label='Start Date' component={PortDate} />
+//           <Field
+//             name='endDate'
+//             label='End Date'
+//             canBeDisabled={true}
+//             component={PortDate}
+//           />
+
+//           <Button
+//             style={{
+//               backgroundColor: "#222",
+//             }}
+//             type='submit'
+//             disabled={isSubmitting}
+//           >
+//             Create
+//           </Button>
+//         </Form>
+//       )}
+//     </Formik>
+//   </div>
+// )
+
+// export default PortfolioCreatForm
--- pages/portfolionew.js
+++ pages/portfolionew.js
@@ -3,8 +3,17 @@ import BasePage from "@components/BasePage"
 import BaseLayout from "@layouts/BaseLayout"
 import withAuth from "@hoc/withAuth"
 import PortfolioCreatForm from "@components/portfolios/PorfolioCreateForm"
+import { Row, Col } from "reactstrap"
 
 class PortfolioNew extends React.Component {
+  constructor(props) {
+    super()
+
+    this.savePortfolio = this.savePortfolio.bind(this)
+  }
+  savePortfolio(portfolioData) {
+    alert(JSON.stringify(portfolioData, null, 2))
+  }
   render() {
     return (
       <BaseLayout {...this.props.auth}>
@@ -12,7 +21,11 @@ class PortfolioNew extends React.Component {
           className='portfolio-create-page'
           title='Create new Portfolio'
         >
-          <PortfolioCreatForm />
+          <Row>
+            <Col md='6'>
+              <PortfolioCreatForm onSubmit={this.savePortfolio} />
+            </Col>
+          </Row>
         </BasePage>
       </BaseLayout>
     )
