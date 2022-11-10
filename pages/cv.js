1df191b7f0080d49fe0138038cecea395d1fb034
adenugbamichael <adenugbamicke@gmail.com>
2022-11-10 11:28:55 +0100

    Created a CV page and made few fixes on page

--- pages/cv.js
+++ pages/cv.js
@@ -2,12 +2,30 @@ import React from "react"
 import BasePage from "@components/BasePage"
 import BaseLayout from "@layouts/BaseLayout"
 
+import { Row, Col } from "reactstrap"
+
 class Cv extends React.Component {
   render() {
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage>
-          <h1>I am Cv page</h1>
+        <BasePage title='Preview of my CV' className='cv-page'>
+          <Row>
+            <Col md={{ size: 8, offset: 2 }}>
+              <div className='cv-title'>
+                <a
+                  download='live_cv.pdf'
+                  className='btn btn-success'
+                  href='/static/live_cv.pdf'
+                >
+                  Download
+                </a>
+              </div>
+              <iframe
+                style={{ width: "100%", height: "800px" }}
+                src='/static/live_cv.pdf'
+              ></iframe>
+            </Col>
+          </Row>
         </BasePage>
       </BaseLayout>
     )
